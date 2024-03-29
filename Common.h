07d4deabfc6570b36bb04eb1f95f5b5bcb32a1f0
Omid Shahbazi <sh.omid.m@gmail.com>
2024-03-29 19:28:56 +0330

    Added      Bitwise.h Added      Common.h Added      DSPs/AutoWah.h Added      DSPs/Chorus.h Added      DSPs/Compressor.h Added      DSPs/Distortion.h Added      DSPs/Flanger.h Added      DSPs/IDSP.h Added      DSPs/Looper.h Added      DSPs/NoiseGate.h Added      DSPs/Overdrive.h Added      DSPs/Phaser.h Added      DSPs/Reverb.h Added      DSPs/Sustain.h Added      DSPs/Test.h Added      DSPs/Tremolo.h Added      DSPs/Wah.h Added      Debug.h Added      Filters/BandPassFilter.h Added      Filters/BandStopFilter.h Added      Filters/BiquadFilter.h Added      Filters/DelayFilter.h Added      Filters/EnvelopeFollowerFilter.h Added      Filters/Filter.h Added      Filters/HighPassFilter.h Added      Filters/LowPassFilter.h Added      Filters/NoiseGateFilter.h Added      Filters/OscillatorFilter.h Added      Filters/SingleToneControlFilter.h Added      Filters/ThreeToneControlFilter.h Added      Filters/WaveShaperFilter.h Added      Log.h Added      Math.h Added      Memory.h Added      Notes.h Added      SineWaveGenerator.h Added      Time.h

---
+++ Common.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <inttypes.h>
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+
+#define KHz 1000
+#define MHz 1000 * KHz
+
+#define SAMPLE_RATE_8000 8 * KHz
+#define SAMPLE_RATE_15750 15.75 * KHz
+#define SAMPLE_RATE_16000 16 * KHz
+#define SAMPLE_RATE_22050 22.05 * KHz
+#define SAMPLE_RATE_24000 24 * KHz
+#define SAMPLE_RATE_32000 32 * KHz
+#define SAMPLE_RATE_44100 44.1 * KHz
+#define SAMPLE_RATE_48000 48 * KHz
+#define SAMPLE_RATE_96000 96 * KHz
+#define SAMPLE_RATE_192000 192 * KHz
+#define SAMPLE_RATE_320000 320 * KHz
+
+#define MIN_SAMPLE_RATE SAMPLE_RATE_8000
+#define MAX_SAMPLE_RATE SAMPLE_RATE_320000
+
+#define MIN_FREQUENCY 20.0
+#define MAX_FREQUENCY 20.0 * KHz
