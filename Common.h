642109a650ae176b9fb2e90a9bb84b8af516ab0e
Omid Shahbazi <sh.omid.m@gmail.com>
2024-05-21 08:19:25 +0330

    Modified   Common.h

--- Common.h
+++ Common.h
@@ -44,3 +44,46 @@ typedef unsigned long long uint64;
 #else
 #define ARE_TYPES_THE_SAME(A, B) true
 #endif
+
+struct Color
+{
+public:
+	Color(void)
+		: R(0),
+		  G(0),
+		  B(0),
+		  A(255)
+	{
+	}
+
+	Color(uint8 R, uint8 G, uint8 B)
+		: R(R),
+		  G(G),
+		  B(B),
+		  A(255)
+	{
+	}
+
+	Color(uint8 R, uint8 G, uint8 B, uint8 A)
+		: R(R),
+		  G(G),
+		  B(B),
+		  A(A)
+	{
+	}
+
+	uint16 R5G6B5(void)
+	{
+		uint16 b = (B >> 3) & 0x1f;
+		uint16 g = ((G >> 2) & 0x3f) << 5;
+		uint16 r = ((R >> 3) & 0x1f) << 11;
+
+		return (uint16)(r | g | b);
+	}
+
+public:
+	uint8 R;
+	uint8 G;
+	uint8 B;
+	uint8 A;
+};
