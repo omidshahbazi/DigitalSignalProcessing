cd41adf52a783b221a7398aa972fbde83b46bb49
Omid Shahbazi <sh.omid.m@gmail.com>
2026-05-17 17:57:23 -0700

    Modified   Bitwise.h

--- Bitwise.h
+++ Bitwise.h
@@ -8,20 +8,20 @@ class Bitwise
 {
 public:
 	template <typename T, typename U>
-	static bool IsEnabled(T Mask, U Bits)
+	static constexpr bool IsEnabled(T Mask, U Bits)
 	{
 		return (((int32)Mask & (int32)Bits) == (int32)Bits);
 	}
 };
 
 template <typename T, typename U>
-T operator|(T Left, U Right)
+constexpr T operator|(T Left, U Right)
 {
 	return (T)((int32)Left | (int32)Right);
 }
 
 template <typename T, typename U>
-T &operator|=(T &Left, U Right)
+constexpr T &operator|=(T &Left, U Right)
 {
 	Left = (T)((int32)Left | (int32)Right);
 	return Left;
