18af6cee5c9d7dbae67ec4ea697bfe8c01d7baa3
Omid Shahbazi <sh.omid.m@gmail.com>
2026-07-19 19:24:59 -0700

    Modified   DataTypes.h

--- DataTypes.h
+++ DataTypes.h
@@ -1,15 +1,18 @@
 #pragma once
 
-typedef signed char int8;
-typedef short int16;
-typedef int int32;
-typedef long long int64;
+#include <cstdint>
 
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef unsigned long long uint64;
-typedef const char *cstr;
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t  uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef const char* cstr;
 
 #ifdef DEBUG
 #ifdef ENABLE_TYPE_CHECK
