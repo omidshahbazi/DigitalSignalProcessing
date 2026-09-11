7275cf65eac682a4c1d87b5bf94fddd1bd60caf5
Omid Shahbazi <sh.omid.m@gmail.com>
2026-09-11 10:18:51 -0700

    Modified   DigitalSignalProcessing.vcxproj Modified   Include/DigitalSignalProcessing/USB/IUSBCDCInterface.h

--- Include/DigitalSignalProcessing/USB/IUSBCDCInterface.h
+++ Include/DigitalSignalProcessing/USB/IUSBCDCInterface.h
@@ -8,12 +8,23 @@
 class IUSBCDCInterface : public IUSBInterface
 {
 public:
+	enum class TransmitStates
+	{
+		Idle = 0,
+		Busy
+	};
+
 	typedef FixedFunction<void(const uint8_t* Buffer, uint8_t Length)> ReceiveCallback;
+	typedef FixedFunction<void(void)> StateChangedCallback;
 
 public:
 	virtual ~IUSBCDCInterface(void) = default;
 
 	virtual bool IsConnected(void) const = 0;
+	virtual void SetConnectionStateChangedCallback(StateChangedCallback Callback) = 0;
+
+	virtual TransmitStates GetTransmitState(void) const = 0;
+	virtual void SetTransmitStateChangedCallback(StateChangedCallback Callback) = 0;
 
 	virtual void SetReceiveCallback(ReceiveCallback Callback) = 0;
 
